b94d4961f18146ff3aa819f1054a68c39e0c2331
Arend Peter <apc1993@gmail.com>
2025-02-20 17:55:59 -0800

    Move mailchimp tools to their own file

--- src/useCookie.ts
+++ src/useCookie.ts
@@ -17,7 +17,7 @@ function getCookie(name: string) {
         var c = ca[i];
         while (c.charAt(0)==' ') c = c.substring(1,c.length);
         if (c.indexOf(nameEQ) == 0){
-        return c.substring(nameEQ.length,c.length);
+            return c.substring(nameEQ.length,c.length);
         }
     }
     return null;
